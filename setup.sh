d7d6ea5bc16d8526661fae1123c29880625a97a8
AIND Workspaces <aind-workspaces@udacity.com>
2018-04-16 23:43:09 +0000

    Add instructions for Project Assistant submission

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Download your JWT here:"
+echo
+echo "    https://project-assistant.udacity.com/auth_tokens/new"
+echo
+echo "Once you've downloaded your JWT, copy the contents into the editor tab titled 'jwt'"
+echo "After that step, you can run project assistant by running 'udacity submit' from the terminal"
