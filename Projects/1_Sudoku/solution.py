cd961090965e903d2dea3c7f1b29e730aa1b02f9
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-04-25 09:06:24 +0900

    Removed print functions

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -45,17 +45,14 @@ def naked_twins(values):
     strategy repeatedly).
     """
     # TODO: Implement this function!
-    display(values)
     for unit in unitlist:
         for box_key in unit:
             if len(values[box_key]) == 2:
                 occurence = [key for key in unit if values[key] == values[box_key]]
                 if len(occurence) == 2: # found naked twin
-                    print('Naked Twin : ' + str(values[box_key]))
                     for key in unit:
                         if values[key] != values[box_key]: # not one of naked twin
                             values[key] = values[key].replace(values[box_key][0], '').replace(values[box_key][1], '')
-    display(values)
     return values
 
 
