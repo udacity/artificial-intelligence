28e83fd9580f025ba1cd635c99fb33f74ff1df78
Ker Wei Khaw <kerwei84@gmail.com>
2019-01-30 21:27:51 +0800

    Refactor tree_policy to use loops instead of recursion. Fix bug with Stat list pop method. Added addition subtraction modifier flag for further flexibility in testing score propagation

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,10 +1,11 @@
-import logging
+# import logging
 import math
 import pdb
 import random
 
-
+from copy import deepcopy
 from collections import namedtuple
+
 from sample_players import DataPlayer
 
 
@@ -25,8 +26,10 @@ class CustomPlayer(DataPlayer):
     i = 1
     plyturn = None
     statlist = []
+    modifier = 1
+    # opslist = []
     Stat = namedtuple('Stat', 'board plycount locs action utility visit nround')
-    logging.basicConfig(filename='matches.log',level=logging.DEBUG)
+    # logging.basicConfig(filename='matches.log',level=logging.DEBUG)
 
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
@@ -49,21 +52,33 @@ class CustomPlayer(DataPlayer):
         #          call self.queue.put(ACTION) at least once before time expires
         #          (the timer is automatically managed for you)
         self.plyturn = state.ply_count % 2
+        if self.plyturn == 1:
+            self.modifier = -1
         # Just to prevent a forfeit due to a no-action!
         self.queue.put(random.choice(state.actions()))
+        # logging.info("\n-------------------------------------------------\n\n")
+        # logging.info("Play %s\n" % (state.ply_count))
 
         while True:
+            # logging.info("\n-------------------------------------------------\n\n")
+            # logging.info("Play %s\n" % (state.ply_count))
             next_action = self.uct_search(state)
             self.queue.put(next_action)
-            logging.info("Play %s, Round %s\n" % (state.ply_count, self.i))
-            logging.info("Selected action: %s\n" % next_action)
-            logging.info(str(self.statlist))
-            logging.info("\n-------------------------------------------------\n\n")
+            # logging.info(str(self.opslist))
+            # logging.info("Round %s\n" % (self.i))
+            # logging.info("Selected action: %s\n" % next_action)
+            # logging.info(str(self.statlist))
+            # logging.info("\n-------------------------------------------------\n\n")
             self.i += 1
+
+        # logging.info("Selected action: %s\n" % next_action)
+        # logging.info("Rounds Simulated %s\n" % (self.i))
+        # logging.info(str(self.statlist))
+        # logging.info("\n-------------------------------------------------\n\n")
 
 
     def uct_search(self, state):
-        next_state = self.tree_policy(state)
+        next_state = self.tree_policy_nonrecurse(state)
 
         if not next_state.terminal_test():
             delta = self.default_policy(next_state)
@@ -72,49 +87,50 @@ class CustomPlayer(DataPlayer):
         return self.best_child(state, 0)
 
 
-    def tree_policy(self, state):
-        """
-        Determine the most urgent node to expand - balancing areas not explored and areas that look promising
-        """
-        # logging.debug("Executing tree policy")
-        if state.terminal_test():
-            return state
+    def tree_policy_nonrecurse(self, state):
+        statecopy = deepcopy(state) # State 0
 
-        # All taken actions at this depth
-        tried = [s.action for s in self.statlist if s.board == state.board]
-        # See if there's any untried actions left
-        untried = [a for a in state.actions() if a not in tried]
-        topop = []
-        toappend = []
+        while not statecopy.terminal_test():
+            # logging.debug("Start: %s" % str(statecopy))
+            # All taken actions at this depth
+            tried = [s.action for s in self.statlist if s.board == str(statecopy.board)]
+            # logging.debug("Tried: %s" % str(tried))
+            # See if there's any untried actions left
+            untried = [a for a in statecopy.actions() if a not in tried]
+            # logging.debug("Available actions %s\n Untried %s" % (str(statecopy.actions()), str(untried)))
 
-        if len(untried) > 0:
-            action = random.choice(untried)
-            return self.expand(state, action)
-        else:
-            next_action = self.best_child(state, 1)
+            topop = []
+            toappend = []
 
-            for k, s in enumerate(self.statlist):
-                if s.board == str(state.board) and s.action == next_action:
-                    visit1 = self.statlist[k].visit + 1
-                    news = self.statlist[k]._replace(visit=visit1)
-                    news = news._replace(nround=self.i)
-                    
-                    topop.append(k)
-                    toappend.append(news)
+            if len(untried) > 0:
+                next_action = random.choice(untried)
+                statecopy = self.expand(statecopy, next_action)
+                break
+            else:
+                next_action = self.best_child(statecopy, 1)
 
-                    break
+                for k, s in enumerate(self.statlist):
+                    if s.board == str(statecopy.board) and s.action == next_action:
+                        visit1 = self.statlist[k].visit + 1
+                        news = self.statlist[k]._replace(visit=visit1)
+                        news = news._replace(nround=self.i)
 
-            self.update_scores(topop, toappend)
-            state = self.tree_policy(state.result(next_action))
+                        topop.append(k)
+                        toappend.append(news)
+                        break
 
-            return state
+                self.update_scores(topop, toappend)
+                # logging.debug(str(self.statlist))
+                statecopy = statecopy.result(next_action)
+
+            # logging.debug("\nEnd: %s" % str(statecopy))
+        return statecopy
 
 
     def expand(self, state, action):
         """
         Returns a state resulting from taking an action from the list of untried nodes
         """
-        # logging.debug("Expanding untried nodes")
         self.statlist.append(self.Stat(str(state.board), state.ply_count, state.locs, action, 0, 1, self.i))
         return state.result(action)
 
@@ -124,19 +140,21 @@ class CustomPlayer(DataPlayer):
         Returns the state resulting from taking the best action
         c value between 0 (max score) and 1 (prioritize exploration)
         """
-        # logging.debug("Getting the best child")
         # All taken actions at this depth
         tried = [s for s in self.statlist if s.board == str(state.board)]
-        # Init the score to the score of the first element
-        # TODO: Causing the agent to return only move index 0 at first move
+        # logging.debug("Tried: %s" % str(tried))
+
         maxscore = -2
         maxaction = []
         # Compute the score
         for t in tried:
             score = (t.utility/t.visit) + c * math.sqrt(2 * math.log(self.i)/t.visit)
-            logging.debug("Action %s Score %s" % (t.action, str(score)))
-            if score >= maxscore:
+            # logging.debug("Action %s Score %s" % (t.action, str(score)))
+            if score > maxscore:
                 maxscore = score
+                del maxaction[:]
+                maxaction.append(t.action)
+            elif score == maxscore:
                 maxaction.append(t.action)
 
         return random.choice(maxaction)
@@ -154,7 +172,7 @@ class CustomPlayer(DataPlayer):
             delta = -1
         elif abs(delta) == float('inf') and delta > 0:
             delta = 1
-        logging.info("%s: %s" % (str(self.player_id), delta))
+        # logging.debug("%s: %s" % (str(self.player_id), delta))
         return delta
 
 
@@ -162,16 +180,15 @@ class CustomPlayer(DataPlayer):
         """
         Propagates the terminal utility up the search tree
         """
-        # logging.debug("Propagating terminal value up the tree")
         topop = []
         toappend = []
         for k, s in enumerate(self.statlist):
             if s.nround == self.i:
                 if s.plycount % 2 == self.plyturn:
-                    utility1 = s.utility + delta
+                    utility1 = s.utility + delta * self.modifier
                     news = self.statlist[k]._replace(utility=utility1)
                 elif s.plycount % 2 != self.plyturn:
-                    utility1 = s.utility - delta
+                    utility1 = s.utility - delta * self.modifier
                     news = self.statlist[k]._replace(utility=utility1)
 
                 topop.append(k)
@@ -183,8 +200,8 @@ class CustomPlayer(DataPlayer):
 
 
     def update_scores(self, topop, toappend):
-        # Remove outdated tuples
-        for p in topop:
+        # Remove outdated tuples. Order needs to be in reverse or pop will fail!
+        for p in sorted(topop, reverse=True):
             self.statlist.pop(p)
         # Add the updated ones
         for a in toappend:
