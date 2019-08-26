8f32d5bd07c56e7c6c0044ba844f4cf9b8e459e0
Antoine Mertz <antoine.mertz@ge.com>
2019-08-26 16:16:32 +0200

    exercice|add function to display sudoku

--- Exercises/1_Constraint Satisfaction/util.py
+++ Exercises/1_Constraint Satisfaction/util.py
@@ -76,3 +76,19 @@ def displayBoard(locations, shape):
 
     plt.show()
     return fig
+
+def display_sudoku(values):
+    """Display the values as a 2-D grid.
+
+    Parameters
+    ----------
+        values(dict): The sudoku in dictionary form
+    """
+    boxes = values.keys()
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    print()
