c4e2e42a1701a91866c7c0a359b70d345ea7f88b
Raymond S <raysung78@gmail.com>
2022-12-24 13:36:59 -0500

    feat: final optimization.

--- Projects/1_Sudoku/solution.py
+++ Projects/1_Sudoku/solution.py
@@ -58,26 +58,26 @@ def naked_twins(values):
     new_values = dict(values)
     import collections
     for unit in unitlist:
-        naked_candidates = collections.defaultdict(list)
         # build a {"35" - > [B2,B3]} dictionary
+        naked_candidates = collections.defaultdict(list)
         for box in unit:
             if (len(new_values[box])> 1):
                 naked_candidates[new_values[box]].append(box)
-        if naked_candidates:
-            print(naked_candidates)
+        # if naked_candidates:
+        #     print(naked_candidates)
         # for naked twins/triplet to work, # of boxes must match # of possible values
         filtered_canidates = { possible_values: boxes
                                for possible_values, boxes in naked_candidates.items()
                                if len(possible_values) == len(boxes)}
-        if filtered_canidates:
-            print(filtered_canidates)
+        # if filtered_canidates:
+        #     print(filtered_canidates)
         # identify naked candidate:
         for possible_values, candidate_boxes in filtered_canidates.items():
             for box in unit:
+                # remove the twin/triplets values from the list of possible values for all other unsolved boxes
                 if box not in candidate_boxes and len(new_values[box]) > 1:
-                    # remove the twin/triplets values from the list of possible values for all other unsolved boxes
                     new_values[box] = "".join([digit for digit in new_values[box] if digit not in possible_values])
-                    print('removing {} from {} => values {} -> {}'.format(possible_values, box, values[box], new_values[box]))
+                    # print('removing {} from {} => values {} -> {}'.format(possible_values, box, values[box], new_values[box]))
     return new_values
 
 def eliminate(values):
@@ -154,6 +154,8 @@ def reduce_puzzle(values):
         values = eliminate(values)
         # Use the Only Choice Strategy
         values = only_choice(values)
+        # apply naked elimination:
+        values = naked_twins(values)
         # Check how many boxes have a determined value, to compare
         solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
         # If no new values were added, stop the loop.
@@ -223,7 +225,6 @@ if __name__ == "__main__":
     #diag_sudoku_grid = '.8.1.5...1..849.......3.....5..14..94...8...58..25..3.....9.......473..8...5.1.9.'
     #diag_sudoku_grid = '9.1....8.8.5.7..4.2.4....6...7......5..............83.3..6......9................'
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
-    display(grid2values(diag_sudoku_grid))
     result = solve(diag_sudoku_grid)
     display(result)
 
